7eba7058c7137cb5e6339f78710f0b343b235642
Скорик Виталий22 <vitalik545.GT@yandex.ru>
2023-04-13 09:47:53 +0300

    Добавьте файлы проекта.

---
+++ cockroachRace/Race.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cockroachRace
+{
+    public class Race
+    {
+        private static Tarcoon[] tarcoons;
+        private static int MaxSiz; 
+        private Race() 
+        { 
+        
+        }
+        
+        public static void CreateTarcoons(int quantityOfTarcoonns, int maxSpeed, int minSpeed)
+        {
+            tarcoons = new Tarcoon[quantityOfTarcoonns];
+            for (int i = 0; i < quantityOfTarcoonns; i++)
+            {
+                Random random = new Random();
+                tarcoons[0] = new Tarcoon(random.Next(minSpeed, maxSpeed));
+            }
+        }
+        public static void CreateField(int ciels)
+        {
+            for (int i = 0; i < tarcoons.Length; i++)
+            {
+            }
+        }
+    }
+}
